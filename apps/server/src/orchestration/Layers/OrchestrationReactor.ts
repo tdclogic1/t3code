e1afd5cdbcb0bef4a796c3f5cbaebd5fb6630219
tdclogic1 <kenai45@gmail.com>
2026-03-08 14:19:41 -0400

    Update OrchestrationReactor.ts

--- apps/server/src/orchestration/Layers/OrchestrationReactor.ts
+++ apps/server/src/orchestration/Layers/OrchestrationReactor.ts
@@ -5,6 +5,7 @@ import {
   type OrchestrationReactorShape,
 } from "../Services/OrchestrationReactor.ts";
 import { CheckpointReactor } from "../Services/CheckpointReactor.ts";
+import { ControlPlaneSyncReactor } from "../Services/ControlPlaneSyncReactor.ts";
 import { ProviderCommandReactor } from "../Services/ProviderCommandReactor.ts";
 import { ProviderRuntimeIngestionService } from "../Services/ProviderRuntimeIngestion.ts";
 
@@ -12,11 +13,13 @@ export const makeOrchestrationReactor = Effect.gen(function* () {
   const providerRuntimeIngestion = yield* ProviderRuntimeIngestionService;
   const providerCommandReactor = yield* ProviderCommandReactor;
   const checkpointReactor = yield* CheckpointReactor;
+  const controlPlaneSyncReactor = yield* ControlPlaneSyncReactor;
 
   const start: OrchestrationReactorShape["start"] = Effect.gen(function* () {
     yield* providerRuntimeIngestion.start;
     yield* providerCommandReactor.start;
     yield* checkpointReactor.start;
+    yield* controlPlaneSyncReactor.start;
   });
 
   return {
